5f44987350ef194952c23196928e9e2bf39b960c
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-02-22 22:39:04 -0300

    feat: Changed structure of application

--- router/routes.go
+++ router/routes.go
@@ -2,16 +2,16 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/rainanDeveloper/gopportunities/handler"
+	opportunity_handler "github.com/rainanDeveloper/gopportunities/handler/opportunity"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	api := router.Group("/api")
 	{
-		api.GET("/opportunity", handler.ShowOpportunityHandler)
-		api.POST("/opportunity", handler.CreateOpportunityHandler)
-		api.PUT("/opportunity", handler.UpdateOpportunityHandler)
-		api.DELETE("/opportunity", handler.DeleteOpportunityHandler)
-		api.GET("/opportunities", handler.ListOpportunitiesHandler)
+		api.GET("/opportunity_handler", opportunity_handler.ShowOpportunityHandler)
+		api.POST("/opportunity_handler", opportunity_handler.CreateOpportunityHandler)
+		api.PUT("/opportunity_handler", opportunity_handler.UpdateOpportunityHandler)
+		api.DELETE("/opportunity_handler", opportunity_handler.DeleteOpportunityHandler)
+		api.GET("/opportunities", opportunity_handler.ListOpportunitiesHandler)
 	}
 }
