31f758229bb8ecc0effe8cbacef8a3b4b4cf4ec4
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-02-22 22:52:03 -0300

    feat: Initialized basic config package

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/rainanDeveloper/gopportunities/router"
+import (
+	"github.com/rainanDeveloper/gopportunities/config"
+	"github.com/rainanDeveloper/gopportunities/router"
+)
 
 func main() {
+	error := config.Init()
+	if error != nil {
+		panic(error)
+	}
 	router.Initialize()
 }
